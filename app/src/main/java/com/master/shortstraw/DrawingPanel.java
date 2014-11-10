0db2bba4b71351e8e27366f7696c6717ee61d75a
Maxence Bobin <maxence.bobin@hotmail.fr>
2014-11-10 11:30:09 +0100

    Create a class for the shortstraw algorith It works. Improvements : add buttons to see resampled poitns, line, not resampled points and corners Implement the shpae detection Improve the algorith

--- app/src/main/java/com/master/shortstraw/DrawingPanel.java
+++ app/src/main/java/com/master/shortstraw/DrawingPanel.java
@@ -1,24 +1,20 @@
 package com.master.shortstraw;
 
 import android.content.Context;
-import android.gesture.GesturePoint;
-import android.gesture.GestureUtils;
-import android.gesture.OrientedBoundingBox;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.PorterDuff;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 /**
  * Created by Maxence Bobin on 05/11/14.
@@ -26,21 +22,12 @@ import java.util.List;
 public class DrawingPanel extends View {
 
 
-    private ArrayList<GesturePoint> pointList = new ArrayList<GesturePoint>();
-    private ArrayList<PointF> resamplePointList = new ArrayList<PointF>();
-
-    float distance = 0;
-
-    private int interSpacingConstant = 40;
-    private double boundingBoxDiagonal = 0;
-    private double interSpacingDistance = 0;
-
-    private long startDrawingTime = 0;
+    private ArrayList<PointF> pointList = new ArrayList<PointF>();
 
     //drawing path
     private Path drawPath;
     //drawing and canvas paint
-    private Paint drawPaint, canvasPaint;
+    private Paint drawPaintLine, drawPaintPoints, drawPaintCorners, canvasPaint;
     //initial color
     private int paintColor = 0xFF660000;
     //canvas
@@ -55,15 +42,36 @@ public class DrawingPanel extends View {
 
     private void setupDrawing() {
         drawPath = new Path();
-        drawPaint = new Paint();
+
+        //Set the drawPaintLine
+        drawPaintLine = new Paint();
         //Set the color of the stroke
-        drawPaint.setColor(paintColor);
+        drawPaintLine.setColor(paintColor);
         //Set the initial path properties
-        drawPaint.setAntiAlias(true);
-        drawPaint.setStrokeWidth(20);
-        drawPaint.setStyle(Paint.Style.STROKE);
-        drawPaint.setStrokeJoin(Paint.Join.ROUND);
-        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        drawPaintLine.setAntiAlias(true);
+        drawPaintLine.setStrokeWidth(20);
+        drawPaintLine.setStyle(Paint.Style.STROKE);
+        drawPaintLine.setStrokeJoin(Paint.Join.ROUND);
+        drawPaintLine.setStrokeCap(Paint.Cap.ROUND);
+
+        //Set the drawPaintPoints
+        drawPaintPoints = new Paint();
+        drawPaintPoints.setColor(0xFF4466DD);
+        drawPaintPoints.setAntiAlias(true);
+        drawPaintPoints.setStrokeWidth(20);
+        drawPaintPoints.setStyle(Paint.Style.STROKE);
+        drawPaintPoints.setStrokeJoin(Paint.Join.ROUND);
+        drawPaintPoints.setStrokeCap(Paint.Cap.ROUND);
+
+
+        //Set the drawPaintCorner
+        drawPaintCorners = new Paint();
+        drawPaintCorners.setColor(0xFF66DD00);
+        drawPaintCorners.setAntiAlias(true);
+        drawPaintCorners.setStrokeWidth(20);
+        drawPaintCorners.setStyle(Paint.Style.STROKE);
+        drawPaintCorners.setStrokeJoin(Paint.Join.ROUND);
+        drawPaintCorners.setStrokeCap(Paint.Cap.ROUND);
 
         canvasPaint = new Paint(Paint.DITHER_FLAG);
 
@@ -85,7 +93,7 @@ public class DrawingPanel extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-        canvas.drawPath(drawPath, drawPaint);
+        canvas.drawPath(drawPath, drawPaintLine);
     }
 
     @Override
@@ -95,42 +103,28 @@ public class DrawingPanel extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                startDrawingTime = System.currentTimeMillis();
                 drawPath.moveTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_MOVE:
-                long time = System.currentTimeMillis() - startDrawingTime;
-                pointList.add(new GesturePoint(touchX, touchY, time));
-                drawCanvas.drawPoint(touchX, touchY, drawPaint);
+                pointList.add(new PointF(touchX, touchY));
                 drawPath.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
-                //Reset drawingTime
-                startDrawingTime = 0;
+                //Draw the stroke
+                drawCanvas.drawPath(drawPath, drawPaintLine);
 
-                //drawCanvas.drawPath(drawPath, drawPaint);
+                //Draw the points not sampled
+                for (PointF po : pointList) {
+                    drawCanvas.drawPoint(po.x, po.y, drawPaintPoints);
+                }
 
-                //Get the top left corner point and the bottom right corner point
-                PointF[] points = getPointsBoundingBox();
-                PointF topLeftPoint = points[0];
-                PointF bottomRightPoint = points[1];
+                ShortStraw ss = new ShortStraw();
+                ArrayList<PointF> cornerPoints = ss.getCornerPoints(pointList);
 
-                RectF boundingBox = new RectF(topLeftPoint.x, topLeftPoint.y, bottomRightPoint.x, bottomRightPoint.y);
-
-                //Get the boundingBox diagonal
-                boundingBoxDiagonal = getBoundingBoxDiagonal(boundingBox);
-
-                //Compute the insertSpacing distance
-                interSpacingDistance = boundingBoxDiagonal / interSpacingConstant;
-
-                Paint p = new Paint();
-                p.setColor(0xFF66DD00);
-                p.setAntiAlias(true);
-                p.setStrokeWidth(20);
-                p.setStyle(Paint.Style.STROKE);
-                p.setStrokeJoin(Paint.Join.ROUND);
-                p.setStrokeCap(Paint.Cap.ROUND);
-                drawCanvas.drawRect(boundingBox, p);
+                //Draw corners
+                for (PointF po : cornerPoints) {
+                    drawCanvas.drawPoint(po.x, po.y, drawPaintCorners);
+                }
 
                 drawPath.reset();
                 pointList.clear();
@@ -141,40 +135,6 @@ public class DrawingPanel extends View {
 
         invalidate();
         return true;
-    }
-
-    private double getBoundingBoxDiagonal (RectF box) {
-        //Apply Pythagore
-        double height = box.height();
-        double width = box.width();
-        double diag = Math.sqrt(height*height + width*width);
-        return diag;
-    }
-
-    /**
-     *
-     * @return the top left corner point and the bottom right corner point of a point list
-     */
-    private PointF[] getPointsBoundingBox () {
-        float minX = pointList.get(0).x;
-        float maxX = pointList.get(0).x;
-        float minY = pointList.get(0).y;
-        float maxY = pointList.get(0).y;
-        for (GesturePoint p : pointList) {
-            if (p.x < minX) {
-                minX = p.x;
-            }
-            if (p.x > maxX) {
-                maxX = p.x;
-            }
-            if (p.y < minY) {
-                minY = p.y;
-            }
-            if (p.y > maxY) {
-                maxY = p.y;
-            }
-        }
-        return new PointF[]{new PointF(minX, minY), new PointF(maxX, maxY)};
     }
 
     public void reset () {
---
+++ app/src/main/java/com/master/shortstraw/ShortStraw.java
@@ -0,0 +1,285 @@
+package com.master.shortstraw;
+
+import android.graphics.PointF;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by Maxence Bobin on 10/11/14.
+ */
+public class ShortStraw {
+
+    private int INTERSPACING_CONSTANT = 40;
+    private int STRAW_WINDOW = 3;
+    private double MEDIAN_TRESHOLD = 0.95;
+    private double LINE_TGRESHOLD = 0.95;
+
+    public ShortStraw () {
+    }
+
+    /**
+     * @param pList
+     * @return the corners points according to the pList
+     */
+    public ArrayList<PointF> getCornerPoints(ArrayList<PointF> pList) {
+        if (pList.isEmpty() || pList.size() < 2) {
+            return null;
+        }
+        float interSpacingDistance = (float) determineResamplingSpacing(pList);
+        ArrayList<PointF> resampledPoints = resampling(pList, interSpacingDistance);
+        ArrayList<Integer> indicesPoints = getCorners(resampledPoints);
+        ArrayList<PointF> cornerPoints = new ArrayList<PointF>();
+        for (int i = 0; i < indicesPoints.size(); i++) {
+            PointF p = resampledPoints.get(indicesPoints.get(i));
+            cornerPoints.add(new PointF(p.x, p.y));
+        }
+        return cornerPoints;
+    }
+
+    /**
+     * @param pList
+     * @return an ArraList of the corners indices
+     */
+    public ArrayList<Integer> getCornersIndices (ArrayList<PointF> pList) {
+        if (pList.isEmpty() || pList.size() < 2) return null;
+        float interSpacingDistance = (float) determineResamplingSpacing(pList);
+        ArrayList<PointF> resampledPoints = resampling(pList, interSpacingDistance);
+        return getCorners(resampledPoints);
+    }
+
+    /**
+     * @param pList
+     * @return the resampling spacing according to the points List.
+     */
+    private double determineResamplingSpacing (ArrayList<PointF> pList) {
+        //Get the top left corner point and the bottom right corner point
+        PointF[] points = getPointsBoundingBox(pList);
+        PointF topLeftPoint = points[0];
+        PointF bottomRightPoint = points[1];
+
+        //RectF boundingBox = new RectF(topLeftPoint.x, topLeftPoint.y, bottomRightPoint.x, bottomRightPoint.y);
+
+        //Get the boundingBox diagonal
+        double boundingBoxDiagonal = distance(topLeftPoint, bottomRightPoint);
+
+        //Compute the insertSpacing distance
+        return (boundingBoxDiagonal / INTERSPACING_CONSTANT);
+    }
+
+    /**
+     * @param pointList : The list of points to be resampled
+     * @return a new ArrayList with the resampled points
+     */
+    private ArrayList<PointF> resampling (ArrayList<PointF> pointList, float interSpacingDistance) {
+        float distance = 0;
+        //Create an empty ArrayList of points
+        ArrayList<PointF> resamplePointList = new ArrayList<PointF>();
+        //Add the first point of the stroke on the resampled points
+        resamplePointList.add(pointList.get(0));
+        for (int i = 1; i < pointList.size(); i++) {
+            PointF point1 = pointList.get(i - 1);
+            PointF point2 = pointList.get(i);
+            float d = (float) distance(point1, point2);
+
+            if ((distance+d) >= interSpacingDistance) {
+                PointF newPoint = new PointF();
+                newPoint.x = point1.x + ((interSpacingDistance - distance) / d) * (point2.x - point1.x);
+                newPoint.y = point1.y + ((interSpacingDistance - distance) / d) * (point2.y - point1.y);
+                resamplePointList.add(newPoint);
+                pointList.add(i, newPoint);
+                distance = 0;
+            }
+            else {
+                distance += d;
+            }
+        }
+        return resamplePointList;
+    }
+
+    /**
+     * @param resamplePointList
+     * @return the corners indexes within the stroke
+     */
+    private ArrayList<Integer> getCorners (ArrayList<PointF> resamplePointList) {
+        //Create a new ArrayList for the corners indexes
+        ArrayList<Integer> corners = new ArrayList<Integer>();
+        corners.add(0);
+        //Create a new ArrayList for the straws
+        ArrayList<Float> straws = new ArrayList<Float>();
+
+        //Add the distance between the i - STRAW_WINDOW and i + STRAW_WINDOW points
+        for (int i = STRAW_WINDOW; i < resamplePointList.size() - STRAW_WINDOW; i++) {
+            PointF point1 = resamplePointList.get(i - STRAW_WINDOW);
+            PointF point2 = resamplePointList.get(i + STRAW_WINDOW);
+            straws.add((float) distance(point1, point2));
+        }
+
+        //Calculate the median of the straws
+        float t = (float) (median(straws) * MEDIAN_TRESHOLD);
+
+        for (int i = STRAW_WINDOW; i < resamplePointList.size() - STRAW_WINDOW; i++) {
+            float s = straws.get(i - STRAW_WINDOW);
+            //If straw < median
+            if (s < t) {
+                double localMin = Double.POSITIVE_INFINITY;
+                int localMinIndex = i;
+                //Retrieve the local min
+                while (i < straws.size() && s < t) {
+                    if (s < localMin) {
+                        localMin = s;
+                        localMinIndex = i;
+                    }
+                    i++;
+                    s = straws.get(i - STRAW_WINDOW);
+                }
+                //Add the local min index to the array list
+                corners.add(localMinIndex);
+            }
+        }
+        //Add the last index
+        corners.add(resamplePointList.size() - 1);
+        corners = postProcessCorners(resamplePointList, corners, straws);
+        return corners;
+    }
+
+    /**
+     * @param values
+     * @return the median of an array List of float
+     */
+    private float median (ArrayList<Float> values) {
+        //Sort the array List into ascending order
+        Collections.sort(values);
+        int m;
+        if (values.size() / 2 == 0) {
+            m = values.size() / 2;
+            return (values.get(m - 1) + values.get(m))/2;
+        }
+        else {
+            m = (values.size() + 1) / 2;
+            return values.get(m - 1);
+        }
+    }
+
+    /**
+     * @param pList
+     * @param corners
+     * @param straws
+     * @return the arrayList of corners indexes  after checking if any corners
+     * can be removed or added based on high level polyline rules
+     */
+    private ArrayList<Integer> postProcessCorners (ArrayList<PointF> pList, ArrayList<Integer> corners, ArrayList<Float> straws) {
+        boolean go = false;
+        int c1, c2, i;
+        while (!go) {
+            go = true;
+            for (i = 1; i < corners.size(); i++) {
+                c1 = corners.get(i - 1);
+                c2 = corners.get(i);
+                if (!isLine(pList, c1, c2)) {
+                    int newCorner = halfWayCorner(straws, c1, c2);
+                    if (newCorner > c1 && newCorner < c2) {
+                        corners.add(i, newCorner);
+                        go = false;
+                    }
+                }
+            }
+        }
+        for (i = 1; i < corners.size() - 1; i++) {
+            c1 = corners.get(i - 1);
+            c2 = corners.get(i + 1);
+            if (isLine(pList, c1, c2)) {
+                corners.remove(i);
+                i--;
+            }
+        }
+        return corners;
+    }
+
+    /**
+     * Determines if the stroke segment between the points
+     * indices c1 and c2 form a line
+     * @param pList
+     * @param c1
+     * @param c2
+     * @return a boolean
+     */
+    private boolean isLine (ArrayList<PointF> pList, int c1, int c2) {
+        float distance = (float) distance(pList.get(c1), pList.get(c2));
+        float pathDistance = pathDistance(pList, c1, c2);
+        return (distance / pathDistance) > LINE_TGRESHOLD;
+    }
+
+    /**
+     * Computes the euclidian path distance between the points at indices c1 and c2
+     * @param pList
+     * @param c1
+     * @param c2
+     * @return
+     */
+    private float pathDistance (ArrayList<PointF> pList, int c1, int c2) {
+        float d = 0;
+        for (int i = c1; i < c2; i++) {
+            d += distance(pList.get(i), pList.get(i+1));
+        }
+        return d;
+    }
+
+    /**
+     * Finds a corner roughly halfway between point indices c1 and c2
+     * @param straws
+     * @param c1
+     * @param c2
+     * @return
+     */
+    private int halfWayCorner (ArrayList<Float> straws, int c1, int c2) {
+        int quarter = (c2 - c1) / 4;
+        double minValue = Double.POSITIVE_INFINITY;
+        int minIndex = 0;
+        for (int i = c1 + quarter; i < c2 - quarter; i++) {
+            float s = straws.get(i - STRAW_WINDOW);
+            if (s < minValue) {
+                minValue = s;
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    /**
+     * @return the top left corner point and the bottom right corner point of a point list
+     */
+    private PointF[] getPointsBoundingBox (ArrayList<PointF> pointList) {
+        float minX = pointList.get(0).x;
+        float maxX = pointList.get(0).x;
+        float minY = pointList.get(0).y;
+        float maxY = pointList.get(0).y;
+        for (PointF p : pointList) {
+            if (p.x < minX) {
+                minX = p.x;
+            }
+            if (p.x > maxX) {
+                maxX = p.x;
+            }
+            if (p.y < minY) {
+                minY = p.y;
+            }
+            if (p.y > maxY) {
+                maxY = p.y;
+            }
+        }
+        return new PointF[]{new PointF(minX, minY), new PointF(maxX, maxY)};
+    }
+
+    /**
+     *
+     * @param p1 : the first 2D point
+     * @param p2 : the second 2D point
+     * @return the euclidian distance between the 2D points p1 and p2.
+     */
+    private double distance(PointF p1, PointF p2) {
+        double d = (p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y);
+        return Math.sqrt(d);
+    }
+}
